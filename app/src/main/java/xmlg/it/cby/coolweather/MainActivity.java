645baffa6ba90b1e76bb099b1af2761278c72534
cby-12 <1735368540@qq.com>
2020-06-27 22:42:32 +0800

    First commit

---
+++ app/src/main/java/xmlg/it/cby/coolweather/MainActivity.java
@@ -0,0 +1,14 @@
+package xmlg.it.cby.coolweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
